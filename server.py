372d4cdd92f535a7272bb292050fe1ac75153987
VarshaK <varsha.kamble61@gmail.com>
2024-05-29 15:40:33 +0100

    added docstrings

--- server.py
+++ server.py
@@ -7,18 +7,21 @@ app = Flask("Mathematics Problem Solver")
 
 @app.route("/sum")
 def sum_route():
+    """Addition"""
     num1 = float(request.args.get('num1'))
     num2 = float(request.args.get('num2'))
     return str(addition(num1, num2))
 
 @app.route("/sub")
 def sub_route():
+    """Subtraction"""
     num1 = float(request.args.get('num1'))
     num2 = float(request.args.get('num2'))
     return str(subtraction(num1, num2))
 
 @app.route("/mul")
 def mul_route():
+    """Multiplication"""
     num1 = float(request.args.get('num1'))
     num2 = float(request.args.get('num2'))
     return str(multiplication(num1, num2))
@@ -27,6 +30,6 @@ def mul_route():
 def render_index_page():
     # render index page
     return render_template('index.html')
-    
+ 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
